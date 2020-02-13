94eccf3a439a2413d92dece0780f8ac29abf0df4
tsuandry <60823037+tsuandry@users.noreply.github.com>
2020-02-13 09:45:58 +0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Login from './src/pages/Login';
+
+export default class App extends React.Component {
+  render() {
+      return (
+        <Login />
+      );
+  }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# react_native_expo
+- Installing pre-requisites (Node, npm, git)
+
+$ expo init projectName
+>blank
+
+$expo start
+enter to a file project
+$expo start
